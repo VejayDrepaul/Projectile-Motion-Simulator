97272a7ce7375cbfc66d5bbbc4463d08a7b9fd6e
Vejay Drepaul <vejayddrepaul@gmail.com>
2023-05-21 19:13:01 -0400

    Render of ball and axis are both working

--- C++/main.cpp
+++ C++/main.cpp
@@ -1,80 +1,62 @@
 #include <iostream>
 #include <SDL2/SDL.h>
-#include <tuple>
-#include <math.h>
 
 #define WINDOW_WIDTH 1200
 #define WINDOW_HEIGHT 500
 #define WINDOW_X 410
 #define WINDOW_Y 225
+#define BALL_RADIUS 20
 #define GRAVITY 9.80665 // Force of gravity
 #define INITIAL_X 0
 #define INITIAL_y 0
 
 int main(int argc, char *args[]) {
-    SDL_Window *window = NULL;
-	SDL_Surface *screenSurface = NULL;
+    SDL_Init(SDL_INIT_VIDEO);
+	SDL_Window *window = SDL_CreateWindow("Projectile Motion Simulator", WINDOW_X, WINDOW_Y, 
+                                            WINDOW_WIDTH, WINDOW_HEIGHT, SDL_WINDOW_SHOWN);
+    SDL_Renderer *renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
 
-	if (SDL_Init(SDL_INIT_VIDEO) < 0) {
-		std::cout << "SDL could not be initilazied: " << SDL_GetError() << std::endl;
-	}
-	else {
-		window = SDL_CreateWindow("Projectile Motion Simulator", WINDOW_X, WINDOW_Y, 
-                                WINDOW_WIDTH, WINDOW_HEIGHT, SDL_WINDOW_SHOWN);
+    if (SDL_Init(SDL_INIT_VIDEO) < 0 || window == NULL || !renderer) {
+        std::cout << "Something failed: " << SDL_GetError() << std::endl;
+        SDL_DestroyWindow(window);
+        SDL_Quit();
+        return -1;
+    }
 
-		if (window == NULL) {
-			std::cout << "Window creation failed: " << SDL_GetError() << std::endl;
-		}
-		else {
-			screenSurface = SDL_GetWindowSurface(window);
-			SDL_FillRect(screenSurface, NULL, SDL_MapRGB(screenSurface->format, 0xFF, 0xFF, 0xFF));
-			SDL_UpdateWindowSurface(window);
+    // Main event loop
+    bool quit = false;
+    SDL_Event event;
+    while (!quit) {
+        while (SDL_PollEvent(&event)) {
+            if (event.type == SDL_QUIT) 
+                quit = true;
+        }
 
-            SDL_Renderer *renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
-            if (!renderer) {
-                std::cout << "Renderer creation failed: " << SDL_GetError() << std::endl;
-                SDL_DestroyWindow(window);
-                SDL_Quit();
-                return -1;
+        // Setting background color to white
+        SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
+        SDL_RenderClear(renderer);
+
+        // Rendering in the x-axis and y-axis 
+        SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255); 
+        SDL_RenderDrawLine(renderer, 10, 5, 10, WINDOW_HEIGHT - 10);
+        SDL_RenderDrawLine(renderer, 10, WINDOW_HEIGHT - 10, 1195, WINDOW_HEIGHT - 10);
+
+        // Rendering the ball using the formula x^2 + y^2 = r^2
+        for (int y = -BALL_RADIUS; y <= BALL_RADIUS; ++y) {
+            for (int x = -BALL_RADIUS; x <= BALL_RADIUS; ++x) {
+                if (x * x + y * y <= BALL_RADIUS * BALL_RADIUS) 
+                    SDL_RenderDrawPoint(renderer, 30 + x, (WINDOW_HEIGHT - 30) + y);
             }
+        }
 
-            // setting background color to white
-            SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
-            SDL_RenderClear(renderer);
-            SDL_RenderPresent(renderer);
+        // Update the screen
+        SDL_RenderPresent(renderer);
+    }
 
-            // drawing of vertical x-axis
-            SDL_RenderClear(renderer);
-            SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
-            SDL_RenderDrawLine(renderer, 10, 10, 10, WINDOW_HEIGHT - 10);
-            SDL_RenderPresent(renderer);
-
-            // drawing of horizantal y-axis
-
-			SDL_Event event; 
-			bool quit = false; 
-			while (quit == false) { 
-				while (SDL_PollEvent(&event)) { 
-					if (event.type == SDL_QUIT) 
-						quit = true; 
-				} 
-			}
-		}
-	}
-
-	SDL_DestroyWindow(window);
-	SDL_Quit();
+    // Cleanup and exit
+    SDL_DestroyRenderer(renderer);
+    SDL_DestroyWindow(window);
+    SDL_Quit();
 
     return 0;
 }
-
-// physics calculations: velocity components and time of flight of projectile
-std::tuple<double, double, double> flight_mechanics(double velocity, double angle) {
-    double rad = angle * (M_PI / 180);
-    double vx = velocity * cos(rad); 
-    double vy = velocity * sin(rad);
-    double flight_time = (2 * (velocity * sin(rad))) / GRAVITY;
-
-    return std::make_tuple(vx, vy, flight_time);
-}
-
