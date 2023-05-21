1b9c61fb7205ae3350a3a0fd13ef520505999534
Vejay Drepaul <vejayddrepaul@gmail.com>
2023-05-21 01:23:34 -0400

    Graph axis are rendered in

--- C++/main.cpp
+++ C++/main.cpp
@@ -16,30 +16,40 @@ int main(int argc, char *args[]) {
 	SDL_Surface *screenSurface = NULL;
 
 	if (SDL_Init(SDL_INIT_VIDEO) < 0) {
-		std::cout << "SDL could not be initilazied! SDL Error: " << SDL_GetError() << std::endl;
+		std::cout << "SDL could not be initilazied: " << SDL_GetError() << std::endl;
 	}
 	else {
 		window = SDL_CreateWindow("Projectile Motion Simulator", WINDOW_X, WINDOW_Y, 
                                 WINDOW_WIDTH, WINDOW_HEIGHT, SDL_WINDOW_SHOWN);
 
 		if (window == NULL) {
-			std::cout << "Window could not be created! SDL_Error: " << SDL_GetError() << std::endl;
+			std::cout << "Window creation failed: " << SDL_GetError() << std::endl;
 		}
 		else {
 			screenSurface = SDL_GetWindowSurface(window);
 			SDL_FillRect(screenSurface, NULL, SDL_MapRGB(screenSurface->format, 0xFF, 0xFF, 0xFF));
 			SDL_UpdateWindowSurface(window);
 
-            SDL_Renderer *renderer = SDL_CreateRenderer(window, -1, 0);
+            SDL_Renderer *renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
             if (!renderer) {
                 std::cout << "Renderer creation failed: " << SDL_GetError() << std::endl;
                 SDL_DestroyWindow(window);
                 SDL_Quit();
                 return -1;
             }
-            SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
+
+            // setting background color to white
+            SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
             SDL_RenderClear(renderer);
             SDL_RenderPresent(renderer);
+
+            // drawing of vertical x-axis
+            SDL_RenderClear(renderer);
+            SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
+            SDL_RenderDrawLine(renderer, 10, 10, 10, WINDOW_HEIGHT - 10);
+            SDL_RenderPresent(renderer);
+
+            // drawing of horizantal y-axis
 
 			SDL_Event event; 
 			bool quit = false; 
